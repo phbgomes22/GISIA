97b849ef34bcac0f4adb984ee7a58347b12a1986
Pedro Gomes <pedrogomes@MacBook-Pro-de-Pedro.local>
2025-03-06 20:42:10 -0300

    Add requirement for deploy running. Add load of chromadb in main.py.

--- main.py
+++ main.py
@@ -2,9 +2,22 @@
 from model import DocumentDatabase
 from view import *
 from controller import ChatController
+import subprocess
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 
 
 if __name__ == "__main__":
+    # Load ChromaDB from remote source
+    try:
+        result = subprocess.run(["./loader_vector_db.sh", "--load"], check=True, text=True, capture_output=True)
+        print("****** Script output:", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("****** Error:", e.stderr)
+
     # Initialize MVC components
     model = DocumentDatabase(file_path="data/Dominios sobre impacto socioambiental positivo")
     view = ChatView(file_path="data/Dominios sobre impacto socioambiental positivo")
